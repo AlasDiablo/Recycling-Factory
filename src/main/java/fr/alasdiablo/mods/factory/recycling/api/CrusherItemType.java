41afb2df478b26d2731a60a5a3df4d9a1b8321e6
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2024-04-14 13:00:12 +0200

    feat: change the CrusherItemType inner function

--- src/main/java/fr/alasdiablo/mods/factory/recycling/api/CrusherItemType.java
+++ src/main/java/fr/alasdiablo/mods/factory/recycling/api/CrusherItemType.java
@@ -4,31 +4,41 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CrusherItemType {
+    public record ItemType(Integer itemTypeId, Item drop) {}
+    public record ItemMap(String itemType, Integer maxAmount) {}
+
     private static final AtomicInteger INDEX = new AtomicInteger(0);
-    private static final Map<String, Integer> ITEM_TYPE      = new HashMap<>();
-    private static final Map<String, Item>    ITEM_TYPE_DROP = new HashMap<>();
-    private static final Map<Item, String> ITEM_TO_ITEM_TYPE = new HashMap<>();
+    private static final Map<String, ItemType> ITEM_TYPE = new HashMap<>();
+    private static final Map<Item, ItemMap> ITEM_MAP     = new HashMap<>();
 
     /**
      * Add a new item type
      * @param itemType name of the item type
-     * @return False if the item type has been refuse because it already existe or the item type has reach 127 entries.
      */
-    public static boolean addItemType(String itemType, Item itemDrop) {
+    public static void addItemType(String itemType, Item itemDrop) throws IndexOutOfBoundsException, IllegalStateException {
         if (ITEM_TYPE.size() >= 127) {
-            return false;
+            throw new IndexOutOfBoundsException("The ItemType map is full!");
         }
         if (ITEM_TYPE.get(itemType) != null) {
-            return false;
+            throw new IllegalStateException("The ItemType already exists!");
         }
-        ITEM_TYPE.put(itemType, INDEX.getAndIncrement());
-        return true;
+        ITEM_TYPE.put(itemType, new ItemType(INDEX.getAndIncrement(), itemDrop));
+    }
+
+    public static void addCrushableItem(String itemType, Item crushableItem, int maxAmount) {
+        if (ITEM_TYPE.get(itemType) == null) {
+            throw new IllegalStateException("The ItemType does not exist!");
+        }
+        if (ITEM_MAP.get(crushableItem) != null) {
+            throw new IllegalStateException("The Crushable Item already exists!");
+        }
+        ITEM_MAP.put(crushableItem, new ItemMap(itemType, maxAmount));
     }
 
     @Contract(pure = true)
@@ -36,81 +46,78 @@ public class CrusherItemType {
         return ITEM_TYPE.keySet();
     }
 
-    public static @Nullable Integer getItemTypeIndex(String itemType) {
-        return ITEM_TYPE.get(itemType);
-    }
+    public static @Nullable Integer getItemTypeId(Item crushableItem) {
+        ItemMap entry = ITEM_MAP.get(crushableItem);
+        if (entry == null) {
+            return null;
+        }
 
-    public static @Nullable Item getItemTypeDrop(String itemType) {
-        return ITEM_TYPE_DROP.get(itemType);
-    }
+        ItemType itemType = ITEM_TYPE.get(entry.itemType);
+        if (itemType == null) {
+            return null;
+        }
 
-    public static @Nullable String getItemTypeFromItem(Item item) {
-        return ITEM_TO_ITEM_TYPE.get(item);
+        return itemType.itemTypeId;
     }
 
     @Deprecated
     public static void register() {
-        ITEM_TYPE.put("wooden", INDEX.getAndIncrement());
-        ITEM_TYPE_DROP.put("wooden", Items.STICK);
-        ITEM_TO_ITEM_TYPE.put(Items.WOODEN_AXE, "wooden");
-        ITEM_TO_ITEM_TYPE.put(Items.WOODEN_HOE, "wooden");
-        ITEM_TO_ITEM_TYPE.put(Items.WOODEN_PICKAXE, "wooden");
-        ITEM_TO_ITEM_TYPE.put(Items.WOODEN_SHOVEL, "wooden");
-        ITEM_TO_ITEM_TYPE.put(Items.WOODEN_SWORD, "wooden");
+        addItemType("wooden", Items.STICK);
+        addCrushableItem("wooden", Items.WOODEN_AXE, 12);
+        addCrushableItem("wooden", Items.WOODEN_HOE, 8);
+        addCrushableItem("wooden", Items.WOODEN_PICKAXE, 12);
+        addCrushableItem("wooden", Items.WOODEN_SHOVEL, 4);
+        addCrushableItem("wooden", Items.WOODEN_SWORD, 8);
 
+        ITEM_TYPE.put("iron", new ItemType(INDEX.getAndIncrement(), Items.IRON_NUGGET));
+        addCrushableItem("iron", Items.IRON_AXE, 27);
+        addCrushableItem("iron", Items.IRON_BARS, 3);
+        addCrushableItem("iron", Items.IRON_BOOTS, 36);
+        addCrushableItem("iron", Items.IRON_CHESTPLATE, 72);
+        addCrushableItem("iron", Items.IRON_DOOR, 18);
+        addCrushableItem("iron", Items.IRON_HELMET, 54);
+        addCrushableItem("iron", Items.IRON_HOE, 18);
+        addCrushableItem("iron", Items.IRON_HORSE_ARMOR, 54);
+        addCrushableItem("iron", Items.IRON_LEGGINGS, 63);
+        addCrushableItem("iron", Items.IRON_PICKAXE, 27);
+        addCrushableItem("iron", Items.IRON_SHOVEL, 9);
+        addCrushableItem("iron", Items.IRON_SWORD, 18);
+        addCrushableItem("iron", Items.IRON_TRAPDOOR, 36);
 
-        ITEM_TYPE.put("iron", INDEX.getAndIncrement());
-        ITEM_TYPE_DROP.put("iron", Items.IRON_NUGGET);
-        ITEM_TO_ITEM_TYPE.put(Items.IRON_AXE, "iron");
-        ITEM_TO_ITEM_TYPE.put(Items.IRON_BARS, "iron");
-        ITEM_TO_ITEM_TYPE.put(Items.IRON_BOOTS, "iron");
-        ITEM_TO_ITEM_TYPE.put(Items.IRON_CHESTPLATE, "iron");
-        ITEM_TO_ITEM_TYPE.put(Items.IRON_DOOR, "iron");
-        ITEM_TO_ITEM_TYPE.put(Items.IRON_HELMET, "iron");
-        ITEM_TO_ITEM_TYPE.put(Items.IRON_HOE, "iron");
-        ITEM_TO_ITEM_TYPE.put(Items.IRON_HORSE_ARMOR, "iron");
-        ITEM_TO_ITEM_TYPE.put(Items.IRON_LEGGINGS, "iron");
-        ITEM_TO_ITEM_TYPE.put(Items.IRON_PICKAXE, "iron");
-        ITEM_TO_ITEM_TYPE.put(Items.IRON_SHOVEL, "iron");
-        ITEM_TO_ITEM_TYPE.put(Items.IRON_SWORD, "iron");
-        ITEM_TO_ITEM_TYPE.put(Items.IRON_TRAPDOOR, "iron");
+        ITEM_TYPE.put("golden", new ItemType(INDEX.getAndIncrement(), Items.GOLD_NUGGET));
+        addCrushableItem("golden", Items.GOLDEN_AXE, 27);
+        addCrushableItem("golden", Items.GOLDEN_BOOTS, 36);
+        addCrushableItem("golden", Items.GOLDEN_CHESTPLATE, 72);
+        addCrushableItem("golden", Items.GOLDEN_HELMET, 54);
+        addCrushableItem("golden", Items.GOLDEN_HOE, 18);
+        addCrushableItem("golden", Items.GOLDEN_HORSE_ARMOR, 54);
+        addCrushableItem("golden", Items.GOLDEN_LEGGINGS, 63);
+        addCrushableItem("golden", Items.GOLDEN_PICKAXE, 27);
+        addCrushableItem("golden", Items.GOLDEN_SHOVEL, 9);
+        addCrushableItem("golden", Items.GOLDEN_SWORD, 18);
 
-        ITEM_TYPE.put("golden", INDEX.getAndIncrement());
-        ITEM_TYPE_DROP.put("golden", Items.GOLD_NUGGET);
-        ITEM_TO_ITEM_TYPE.put(Items.GOLDEN_AXE, "golden");
-        ITEM_TO_ITEM_TYPE.put(Items.GOLDEN_BOOTS, "golden");
-        ITEM_TO_ITEM_TYPE.put(Items.GOLDEN_CHESTPLATE, "golden");
-        ITEM_TO_ITEM_TYPE.put(Items.GOLDEN_HELMET, "golden");
-        ITEM_TO_ITEM_TYPE.put(Items.GOLDEN_HOE, "golden");
-        ITEM_TO_ITEM_TYPE.put(Items.GOLDEN_HORSE_ARMOR, "golden");
-        ITEM_TO_ITEM_TYPE.put(Items.GOLDEN_LEGGINGS, "golden");
-        ITEM_TO_ITEM_TYPE.put(Items.GOLDEN_PICKAXE, "golden");
-        ITEM_TO_ITEM_TYPE.put(Items.GOLDEN_SHOVEL, "golden");
-        ITEM_TO_ITEM_TYPE.put(Items.GOLDEN_SWORD, "golden");
+        // TODO Add a drop
+        ITEM_TYPE.put("diamond", new ItemType(INDEX.getAndIncrement(), Items.STONE));
+        addCrushableItem("diamond", Items.DIAMOND_AXE, 27);
+        addCrushableItem("diamond", Items.DIAMOND_BOOTS, 36);
+        addCrushableItem("diamond", Items.DIAMOND_CHESTPLATE, 72);
+        addCrushableItem("diamond", Items.DIAMOND_HELMET, 54);
+        addCrushableItem("diamond", Items.DIAMOND_HOE, 18);
+        addCrushableItem("diamond", Items.DIAMOND_HORSE_ARMOR, 54);
+        addCrushableItem("diamond", Items.DIAMOND_LEGGINGS, 63);
+        addCrushableItem("diamond", Items.DIAMOND_PICKAXE, 27);
+        addCrushableItem("diamond", Items.DIAMOND_SHOVEL, 9);
+        addCrushableItem("diamond", Items.DIAMOND_SWORD, 18);
 
-        ITEM_TYPE.put("diamond", INDEX.getAndIncrement());
-        // Add a drop
-        ITEM_TO_ITEM_TYPE.put(Items.DIAMOND_AXE, "diamond");
-        ITEM_TO_ITEM_TYPE.put(Items.DIAMOND_BOOTS, "diamond");
-        ITEM_TO_ITEM_TYPE.put(Items.DIAMOND_CHESTPLATE, "diamond");
-        ITEM_TO_ITEM_TYPE.put(Items.DIAMOND_HELMET, "diamond");
-        ITEM_TO_ITEM_TYPE.put(Items.DIAMOND_HOE, "diamond");
-        ITEM_TO_ITEM_TYPE.put(Items.DIAMOND_HORSE_ARMOR, "diamond");
-        ITEM_TO_ITEM_TYPE.put(Items.DIAMOND_LEGGINGS, "diamond");
-        ITEM_TO_ITEM_TYPE.put(Items.DIAMOND_PICKAXE, "diamond");
-        ITEM_TO_ITEM_TYPE.put(Items.DIAMOND_SHOVEL, "diamond");
-        ITEM_TO_ITEM_TYPE.put(Items.DIAMOND_SWORD, "diamond");
-
-        ITEM_TYPE.put("netherite", INDEX.getAndIncrement());
-        // Add a drop
-        ITEM_TO_ITEM_TYPE.put(Items.NETHERITE_AXE, "netherite");
-        ITEM_TO_ITEM_TYPE.put(Items.NETHERITE_BOOTS, "netherite");
-        ITEM_TO_ITEM_TYPE.put(Items.NETHERITE_CHESTPLATE, "netherite");
-        ITEM_TO_ITEM_TYPE.put(Items.NETHERITE_HELMET, "netherite");
-        ITEM_TO_ITEM_TYPE.put(Items.NETHERITE_HOE, "netherite");
-        ITEM_TO_ITEM_TYPE.put(Items.NETHERITE_LEGGINGS, "netherite");
-        ITEM_TO_ITEM_TYPE.put(Items.NETHERITE_PICKAXE, "netherite");
-        ITEM_TO_ITEM_TYPE.put(Items.NETHERITE_SHOVEL, "netherite");
-        ITEM_TO_ITEM_TYPE.put(Items.NETHERITE_SWORD, "netherite");
+        ITEM_TYPE.put("netherite", new ItemType(INDEX.getAndIncrement(), Items.NETHERITE_SCRAP));
+        addCrushableItem("netherite", Items.NETHERITE_AXE, 4);
+        addCrushableItem("netherite", Items.NETHERITE_BOOTS, 4);
+        addCrushableItem("netherite", Items.NETHERITE_CHESTPLATE, 4);
+        addCrushableItem("netherite", Items.NETHERITE_HELMET, 4);
+        addCrushableItem("netherite", Items.NETHERITE_HOE, 4);
+        addCrushableItem("netherite", Items.NETHERITE_LEGGINGS, 4);
+        addCrushableItem("netherite", Items.NETHERITE_PICKAXE, 4);
+        addCrushableItem("netherite", Items.NETHERITE_SHOVEL, 4);
+        addCrushableItem("netherite", Items.NETHERITE_SWORD, 4);
     }
 }
